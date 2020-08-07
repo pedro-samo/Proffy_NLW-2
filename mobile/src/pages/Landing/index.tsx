a9bd318ebbc5a4abf04859823e569ecd6819849d
pedrosmorais <pedro.s.morais@gmail.com>
2020-08-07 10:05:01 -0300

    thrid class

---
+++ mobile/src/pages/Landing/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import styles from './styles';
+
+import landingImg from '../../assets/images/landing.png'
+import studyIcon from '../../assets/images/icons/study.png'
+import giveClassIcon from '../../assets/images/icons/give-classes.png'
+import heartIcon from '../../assets/images/icons/heart.png'
+
+
+function Landing() {
+    const { navigate } = useNavigation();
+
+    function handleNavigateToGiveClassesPage() {
+        navigate('GiveClasses')
+    }
+
+    function handleNavigateToStudyPages(){
+        navigate('Study');
+    }
+
+    return (
+        <View style={styles.container}>
+            <Image source={landingImg} style={styles.banner} />
+            <Text style={styles.title}>
+                Seja bem-vindx, {'\n'}
+                <Text style={styles.titleBold}>O que deseja fazer?</Text>
+            </Text>
+
+            <View style={styles.buttonsContainer}>
+
+                <TouchableOpacity 
+                onPress={handleNavigateToStudyPages}
+                style={[styles.button, styles.buttonPrimary]}>
+
+                    <Image source={studyIcon} />
+                    <Text style={styles.buttonText}>Estudar</Text>
+
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={handleNavigateToGiveClassesPage}
+                    style={[styles.button, styles.buttonSecondary]}>
+                    <Image source={giveClassIcon} />
+                    <Text style={styles.buttonText}>Dar Aulas</Text>
+                </TouchableOpacity>
+
+            </View>
+
+            <Text style={styles.totalConnections}>
+                Total de 285 Conexões já realizadas. {' '}
+                <Image source={heartIcon} />
+            </Text>
+
+        </View>
+    );
+}
+
+
+export default Landing; 
